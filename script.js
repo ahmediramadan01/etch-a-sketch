121f00cbdf961761bb22cd462ed3f3944728f146
Ahmed Ramadan <ahmediramadan01@proton.me>
2023-09-21 17:06:56 +0300

    Add grid size slider and Implement render grid functionality on slider value change

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 "user strict";
+
+const sketchpadElement = document.querySelector(".sketchpad");
+const sizeValueElement = document.querySelector("#size-value");
+const sizeSliderElement = document.querySelector("#size-slider");
+
+const pixelElement = document.createElement("div");
+pixelElement.classList.add("pixel");
+pixelElement.style["border"] = "0.01px solid black";
+
+const renderGrid = function () {
+    sketchpadElement.innerHTML = "";
+    const gridSize = sizeSliderElement.value;
+
+    sizeValueElement.textContent = `${gridSize} x ${gridSize}`;
+
+    sketchpadElement.style["grid-template-columns"] = `repeat(${gridSize}, 1fr)`;
+    sketchpadElement.style["grid-template-rows"] = `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            sketchpadElement.insertAdjacentHTML("beforeend", pixelElement.outerHTML);
+        }
+    }
+};
+renderGrid();
+
+sizeSliderElement.addEventListener("change", renderGrid);
+
+sketchpadElement.addEventListener("mouseover", function (event) {
+    event.preventDefault();
+    if (event.target.classList.contains("pixel")) event.target.style.backgroundColor = "black";
+});
